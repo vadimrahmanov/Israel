842d36d4e136d3acf0c2f44083322372cceb13d4
Vadim Rahmanov <VadimRahmanov@gmail.com>
2020-11-06 21:19:14 +0000

    Полностью сделал шапку (все версии), также полностью сделал модальные окна (все версии) с нужным сценарием отзыва. Также завершил блок О программе и начал делать блок Программы

--- build/js/main.js
+++ build/js/main.js
@@ -4,33 +4,89 @@
   var requestModal = document.querySelector('.modal-call-request');
   var requestOpen = document.querySelector('.header__navigation-list-link--call-request');
   var requestClose = document.querySelector('.modal-call-request__close');
+  var requestForm = document.querySelector('.modal-call-request form');
+  var formName = requestForm.querySelector('#name');
+  var formTelephone = requestForm.querySelector('#number');
+  var isStorageSupport = true;
+  var successModal = document.querySelector('.modal-call-success');
+  var successCloseBtn = successModal.querySelector('.modal-call-success__close');
+  var successOkayBtn = successModal.querySelector('.modal-call-success__okay');
+  var storageName = '';
+  var storagePhone = '';
 
-  function openRequestPopup() {
-    requestModal.classList.add('request-open');
+  try {
+    storageName = localStorage.getItem('name');
+    storagePhone = localStorage.getItem('phone');
+  } catch (err) {
+    isStorageSupport = false;
   }
 
-  function closeRequestPopup() {
-    requestModal.classList.remove('request-open');
+  function openPopup(modalWindow) {
+    modalWindow.classList.add('request-open');
+  }
+
+  function closePopup(modalWindow) {
+    modalWindow.classList.remove('request-open');
+  }
+
+  function onSuccess() {
+    closePopup(requestModal);
+    openPopup(successModal);
   }
 
   requestOpen.addEventListener('click', function (e) {
     e.preventDefault();
-    openRequestPopup();
+    openPopup(requestModal);
+    if (storageName) {
+      formName.value = storageName;
+    } else {
+      formName.focus();
+    }
+
+    if (storagePhone) {
+      formTelephone.value = storagePhone;
+    } else {
+      formTelephone.focus();
+    }
   });
 
   requestClose.addEventListener('click', function () {
-    closeRequestPopup();
+    closePopup(requestModal);
   });
 
   document.addEventListener('keydown', function (evt) {
     if (evt.key === 'Escape') {
-      closeRequestPopup();
+      closePopup(requestModal);
+      closePopup(successModal);
     }
   });
 
   document.addEventListener('click', function (e) {
     if (e.target === requestModal) {
-      closeRequestPopup();
+      closePopup(requestModal);
+    }
+  });
+
+  requestForm.addEventListener('submit', function (evt) {
+    evt.preventDefault();
+    if (isStorageSupport) {
+      localStorage.setItem('name', formName.value);
+      localStorage.setItem('phone', formTelephone.value);
+    }
+    onSuccess();
+  });
+
+  successCloseBtn.addEventListener('click', function () {
+    closePopup(successModal);
+  });
+
+  successOkayBtn.addEventListener('click', function () {
+    closePopup(successModal);
+  });
+
+  document.addEventListener('click', function (e) {
+    if (e.target === successModal) {
+      closePopup(successModal);
     }
   });
 })();
